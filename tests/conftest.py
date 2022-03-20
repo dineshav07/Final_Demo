b766b9f7f18ff4408e1a88a9e6e0f990f89fbae0
dinesh <dineshkumarav07@gmail.com>
2022-03-20 20:15:54 +0530

    Testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from seleniumwire import webdriver
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
